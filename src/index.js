3e85ef593eaf8edc6d4efe56a55376cc1777c585
Kajetan Bojko <kajboj@googlemail.com>
2018-04-02 00:06:52 +0100

    added react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Wad from 'web-audio-daw'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {toName, toNumber} from './notes'
 import instrument from './instrument'
 import every from './timing'
@@ -7,5 +9,10 @@ import {addRoot, majorTriad} from './chording'
 const cadence = addRoot(toNumber('C4'), [0, 5, 7, 0]).map((root) => majorTriad(instrument, root))
 
 var clock = every(500, cadence)
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
 
 clock.start()
