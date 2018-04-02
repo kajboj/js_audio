ccdebc466ec0878f82e2f61f1ddfe483c56e4c98
Kajetan Bojko <kajboj@googlemail.com>
2018-04-02 15:27:50 +0100

    every returns a promise

--- src/index.js
+++ src/index.js
@@ -7,12 +7,74 @@ import every from './timing'
 import {addRoot, majorTriad} from './chording'
 
 const cadence = addRoot(toNumber('C4'), [0, 5, 7, 0]).map((root) => majorTriad(instrument, root))
+var playCadence = every(500, cadence)
+var playTone = (toneNumber) => instrument.play(toneNumber)
 
-var clock = every(500, cadence)
+function Tone({tone, chosen}) {
+  return (
+    <button onClick={chosen}>{tone}</button>
+  );
+}
+
+function TonePicker({tones, chosen}) {
+  return tones.map((tone) => <Tone tone={tone} key={tone} chosen={() => chosen(tone)}/>)
+}
+
+class Lesson extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+  }
+
+  generateTone() {
+    this.props.tones[1]
+  }
+
+  startRound() {
+    const tone = generateTone()
+    this.props.playTone(tone)
+    this.setState({
+      tone: tone
+    })
+  }
+
+  isCorrect(guessedTone) {
+    return guessedTone === this.state.tone
+  }
+
+  guess(tone) {
+    if (isCorrect(tone)) {
+      this.props.resolution()
+      this.startRound()
+    } else {
+      this.setState({
+        wrong: true
+      })
+    }
+  }
+
+  giveUp() {
+  }
+
+  render() {
+    return <TonePicker tones={this.props.tones} chosen={(tone) => console.log(tone)}/>
+  }
+}
+
+// cadence
+// guess
+// if (correct)
+//   resolution
+// else
+//   wrong = true
+//   wait for input (give up, cadence, etc.)
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Lesson playCadence={playCadence} playTone={playTone} tones={[1, 2, 3]}/>,
   document.getElementById('root')
-);
+)
 
-clock.start()
+playCadence().then(() => console.log('done'))
--- src/timing.js
+++ src/timing.js
@@ -5,15 +5,26 @@ const wait = (delay) => {
 }
 
 const every = (period, actions) => {
-  return {
-    start: () => {
-      if (actions.length > 0) {
-        actions[0]()
-        wait(period).then(() => {
-          every(period, actions.slice(1)).start()
-        })
-      }
+  let afterLast
+  const p = new Promise((resolve) => afterLast = resolve)
+
+  const newActions = [
+    ...actions,
+    () => afterLast()
+  ]
+
+  const inner = (actions) => {
+    if (actions.length > 0) {
+      actions[0]()
+      wait(period).then(() => {
+        inner(actions.slice(1))
+      })
     }
+  }
+
+  return () => {
+    inner(newActions)
+    return p
   }
 }
 
